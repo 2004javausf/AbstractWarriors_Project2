43af5b616ef1f2ad25765c1f1abe199f73a75318
jpark1914 <parkerj4321@gmail.com>
2020-06-06 20:15:09 -0500

    Working on CORS issue

--- SocialNetwork/src/main/java/com/revature/repos/UsersDAO.java
+++ SocialNetwork/src/main/java/com/revature/repos/UsersDAO.java
@@ -2,13 +2,14 @@ package com.revature.repos;
 
 import java.util.List;
 
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 import com.revature.models.Users;
 
 @Repository
-public interface UsersDAO extends CrudRepository<Users, String>{
+public interface UsersDAO extends CrudRepository<Users, String>, JpaRepository<Users, String>{
 	public List<Users> findUserByUsername(String username);
 	
 	@SuppressWarnings("unchecked")
