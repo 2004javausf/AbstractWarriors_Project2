f56f939c34e78fdfb9fb5edd5abda7ce39887670
nottap1991 <zacharypatton1991@gmail.com>
2020-06-09 17:46:17 -0400

    added methods

--- Trial/SocialNetwork/src/main/java/com/revature/dao/UsersDAO.java
+++ Trial/SocialNetwork/src/main/java/com/revature/dao/UsersDAO.java
@@ -3,6 +3,7 @@ package com.revature.dao;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 import com.revature.entity.Users;
@@ -12,4 +13,14 @@ import com.revature.entity.Users;
 public interface UsersDAO extends JpaRepository<Users, Long>{
 
 	public List<Users> findUsersByUsername(String username);
+	
+	public List<Users> findUsersByUsernameAndPassword(String username, String password);
+	
+	public List<Users> findUsersByFirstName(String firstName);
+	
+	public List<Users> findUsersByUsernameAndEmail(String username, String email);
+
+	
+	
+
 }
